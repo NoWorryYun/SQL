730c1d5eba42b4715ac908ce474610447dce7be7
윤무걱정 <dbstjd3201@naver.com>
2022-05-16 15:23:11 +0900

    나머지 문제도 다 풀었음

--- Practice/Practice03.sql
+++ Practice/Practice03.sql
@@ -28,6 +28,19 @@ and e.job_id = j.job_id
 and e.department_id = d.department_id
 order by employee_id asc;
 
+--문제2-1.
+
+select  e.employee_id,
+        e.first_name,
+        e.salary,
+        d.department_name,
+        j.job_title
+from employees e, jobs j, departments d
+where e.job_id = j.job_id
+and e.department_id = d.department_id(+)
+order by employee_id asc;
+
+
 
 --문제3.
 
@@ -49,6 +62,15 @@ from departments d, locations l
 where d.location_id = l.location_id
 order by location_id asc;
 
+--문제3-1.
+
+select  l.location_id,
+        l.city,
+        d.department_name,
+        d.department_id
+from departments d, locations l
+where d.location_id(+) = l.location_id
+order by location_id asc;
 
 --문제4.
 
@@ -57,6 +79,17 @@ select  r.region_name,
 from countries c, regions r
 where c.region_id=r.region_id
 order by region_name asc, country_name desc;
+
+--문제5.
+select  e.employee_id,
+        e.first_name,
+        e.hire_date,
+        m.first_name,
+        m.hire_date
+from employees e, employees m
+where e.employee_id = m.manager_id
+and e.hire_date > m.hire_date;
+
 
 
 --문제6.
@@ -104,3 +137,23 @@ where d.location_id = l.location_id
 and l.country_id = c.country_id
 and c.region_id = r.region_id
 and e.employee_id = d.manager_id;
+
+
+
+--문제9.
+
+select  e.employee_id,
+        e.first_name,
+        d.department_name,
+        m.first_name
+from employees e, employees m, departments d
+where e.department_id = d.department_id(+)
+and e.manager_id = m.employee_id
+order by e.first_name;
+
+
+
+
+
+
+
