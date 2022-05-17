a3d5ed097c617153e8a5be21538c31f9dd906e39
NoWorryYun <dbstjd3201@naver.com>
2022-05-17 13:37:16 +0900

    subquery >all까지

--- Ex/Ex04.sql
+++ Ex/Ex04.sql
@@ -69,9 +69,85 @@ where salary in (select salary
                  where employee_id = 110);
     
 
+--각 부서별로 최고급여를 받는 사원을 출력하세요
+
+
+--이름을 표현할 수 없다.
+select  max(salary)
+from employees
+group by department_id;
+
+
+--한칸한칸 맞춰서 출력됨
+
+select employee_id,
+        first_name,
+        salary,
+        department_id
+from employees
+where (Department_id, salary) in ( (10, 4400), (20, 13000), (90, 24000) );
+
+
+select  first_name,
+        department_id,
+        salary
+from employees
+where (department_id, salary) IN ( select  department_id,
+                                          max(salary)
+                                  from employees
+                                  group by department_id);
 
 
 
+--ANY
+
+--부서번호가 110인 직원의 급여 보다 큰 모든 직원의 사번, 이름, 급여를 출력하세요. (or 연산 > 8300보다 큰)
+
+select first_name,
+        salary,
+        department_id
+from employees
+where department_id = 110;
+
+select *
+from employees
+where salary > 8300
+or salary > 12008;
+
+
+select  first_Name,
+        salary
+from employees
+where salary >any (select salary
+                   from employees
+                   where department_id = 110);
+
+
+--all <--> any 비교
+-- (1) 부서번호가 110인 직원의 급여 --> 12008 , 8300
+
+select  first_name,
+        salary,
+        department_id
+from employees
+where department_id = 110;
+
+
+select  first_name,
+        salary,
+        department_id
+from employees
+where salary > 8300
+and salary > 12008;
+
+
+select  first_name,
+        salary,
+        department_id
+from employees
+where salary >all ( select  salary
+                    from employees
+                    where department_id = 110);
 
 
 
