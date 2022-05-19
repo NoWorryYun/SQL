048cdd6106b8a08285ae3e80b05c82cd571c35e2
NoWorryYun <dbstjd3201@naver.com>
2022-05-19 17:43:06 +0900

    sequence / commit / rollback // 데이터수정 및 추가

--- Ex/Ex08.sql
+++ Ex/Ex08.sql
@@ -71,3 +71,53 @@ author_id number(10),
 primary key(book_id)
 );
 
+------------------------------------------------
+
+--작가 테이블 삭제
+drop table author;
+
+--작가 시퀀스 삭제
+drop sequence seq_author_id;
+
+--테이블 생성
+create table author(
+    author_id number(10),
+    author_name varchar2(100) not null,
+    author_desc varchar2(100),
+    primary key (author_id)
+);
+
+--작가 sequence 만들기
+create sequence seq_author_id
+increment by 1
+start with 1
+nocache;
+
+--작가 데이터 추가
+insert into author
+values(seq_author_id.nextval, '박경리', '토지작가');
+
+insert into author
+values(seq_author_id.nextval, '이문열', '삼국지작가');
+
+insert into author
+values(seq_author_id.nextval, '기안84', '웹툰작가');
+
+--작가 데이터 수정
+update author
+set author_name = '자취84',
+    author_desc = '나혼자산다 출연'
+where author_id = 3;    
+
+
+
+
+
+
+--작가테이블 조회하기
+select *
+from author;
+
+--작가시퀀스 조회하기
+select *
+from user_sequences;
