057c8d56764bf798f19f3ced11e000726d9cb24f
NoWorryYun <dbstjd3201@naver.com>
2022-05-21 21:34:35 +0900

    6번까지

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -59,29 +59,44 @@ and e.manager_id = m.employee_id;
 
 --문제5.
 
-select  ee.rn,
-        ee.employee_id,
-        ee.first_name,
+select  ort.rn,
+        ort.employee_id,
+        ort.first_name,
         d.department_name,
-        ee.salary,
-        ee.hire_date
-from (select  rownum rn,
-              e.employee_id,
-              e.first_name,
-              e.salary,
-              e.hire_date
-      from (select employee_id,
-                   first_name,
-                   salary,
-                   hire_date
-            from employees
-            order by hire_date asc) e ) ee, departments d
-where ee.department_id = d.department_id
+        ort.salary,
+        ort.hire_date
+from departments d, (select rownum rn,
+                            ot.employee_id,
+                            ot.first_name,
+                            ot.salary,
+                            ot.hire_date,
+                            ot.department_id
+                     from (select employee_id,
+                                  first_name,
+                                  salary,
+                                  hire_date,
+                                  department_id
+                           from employees
+                           where hire_date >= '2005/01/01'
+                           order by hire_date asc) ot
+                     ) ort
+where  ort.department_id = d.department_id(+)
+and rn between 11 and 20
+order by rn asc;                     
+                     
+select *
+from employees
+where hire_date >= '2005/01/01'
 order by hire_date asc;
 
 
+--문제 6.
 
-
-
-
-
+select  first_name||' '||last_name 이름,
+        salary 연봉,
+        d.department_name 부서이름,
+        hire_date
+from employees e, departments d
+where e.department_id = d.department_id(+)
+and hire_date = (select max(hire_date)
+                 from employees);
